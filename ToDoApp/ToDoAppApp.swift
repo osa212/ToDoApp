1148a00ea25571932cbf93482895d2570a7844ef
osa212 <78644067+osa212@users.noreply.github.com>
2021-10-05 20:50:45 +0900

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Sargylana Osipova on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
